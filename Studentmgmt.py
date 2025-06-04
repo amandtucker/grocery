57e5d3ffb6527d64815cc74c24b7f0337bf27a54
amandtucker <88464804+amandtucker@users.noreply.github.com>
2025-06-04 17:17:53 +0530

    Add files via upload

---
+++ Studentmgmt.py
@@ -0,0 +1,16 @@
+# Student   class class room  lecture 
+# account_office fees rollno 
+# libraray office book issue  book return roll no 
+
+class student:
+    def __init__(self, fname, class_name, roll_no):
+        self.name= fname
+        self.Class=class_name
+        self.Rollnumber=roll_no
+        
+    
+    def account_office():
+        
+    def library_office():
+            
+            stu=student()
